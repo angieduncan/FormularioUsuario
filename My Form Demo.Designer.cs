7ab8af06fd91089f82e635dc0d27752ca3cdd74b
Angie Duncan <60791666+angieduncan@users.noreply.github.com>
2021-09-07 11:24:01 -0500

    Cloned and edited

---
+++ My Form Demo.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace Demo_Formulario
+{
+    partial class FrmUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmUsuario));
+            this.Lbltitulo = new System.Windows.Forms.Label();
+            this.LblUsuario = new System.Windows.Forms.Label();
+            this.LblPassword = new System.Windows.Forms.Label();
+            this.LblCorreo = new System.Windows.Forms.Label();
+            this.TxtUsuario = new System.Windows.Forms.TextBox();
+            this.TxtPassword = new System.Windows.Forms.TextBox();
+            this.TxtCorreo = new System.Windows.Forms.TextBox();
+            this.LblPerfil = new System.Windows.Forms.Label();
+            this.CboPerfil = new System.Windows.Forms.ComboBox();
+            this.BtnGuardar = new System.Windows.Forms.Button();
+            this.BtnCancelar = new System.Windows.Forms.Button();
+            this.LstUsuarios = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Lbltitulo
+            // 
+            this.Lbltitulo.AutoSize = true;
+            this.Lbltitulo.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbltitulo.Location = new System.Drawing.Point(82, 19);
+            this.Lbltitulo.Name = "Lbltitulo";
+            this.Lbltitulo.Size = new System.Drawing.Size(96, 27);
+            this.Lbltitulo.TabIndex = 0;
+            this.Lbltitulo.Text = "Usuarios";
+            this.Lbltitulo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // LblUsuario
+            // 
+            this.LblUsuario.AutoSize = true;
+            this.LblUsuario.Location = new System.Drawing.Point(128, 94);
+            this.LblUsuario.Name = "LblUsuario";
+            this.LblUsuario.Size = new System.Drawing.Size(43, 13);
+            this.LblUsuario.TabIndex = 1;
+            this.LblUsuario.Text = "Usuario";
+            // 
+            // LblPassword
+            // 
+            this.LblPassword.AutoSize = true;
+            this.LblPassword.Location = new System.Drawing.Point(128, 157);
+            this.LblPassword.Name = "LblPassword";
+            this.LblPassword.Size = new System.Drawing.Size(53, 13);
+            this.LblPassword.TabIndex = 2;
+            this.LblPassword.Text = "Password";
+            // 
+            // LblCorreo
+            // 
+            this.LblCorreo.AutoSize = true;
+            this.LblCorreo.Location = new System.Drawing.Point(128, 210);
+            this.LblCorreo.Name = "LblCorreo";
+            this.LblCorreo.Size = new System.Drawing.Size(94, 13);
+            this.LblCorreo.TabIndex = 3;
+            this.LblCorreo.Text = "Correo Electronico";
+            // 
+            // TxtUsuario
+            // 
+            this.TxtUsuario.Location = new System.Drawing.Point(267, 94);
+            this.TxtUsuario.Name = "TxtUsuario";
+            this.TxtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.TxtUsuario.TabIndex = 4;
+            // 
+            // TxtPassword
+            // 
+            this.TxtPassword.Location = new System.Drawing.Point(267, 150);
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.PasswordChar = '*';
+            this.TxtPassword.Size = new System.Drawing.Size(100, 20);
+            this.TxtPassword.TabIndex = 5;
+            // 
+            // TxtCorreo
+            // 
+            this.TxtCorreo.Location = new System.Drawing.Point(267, 203);
+            this.TxtCorreo.Name = "TxtCorreo";
+            this.TxtCorreo.Size = new System.Drawing.Size(170, 20);
+            this.TxtCorreo.TabIndex = 6;
+            // 
+            // LblPerfil
+            // 
+            this.LblPerfil.AutoSize = true;
+            this.LblPerfil.Location = new System.Drawing.Point(131, 266);
+            this.LblPerfil.Name = "LblPerfil";
+            this.LblPerfil.Size = new System.Drawing.Size(30, 13);
+            this.LblPerfil.TabIndex = 7;
+            this.LblPerfil.Text = "Perfil";
+            // 
+            // CboPerfil
+            // 
+            this.CboPerfil.FormattingEnabled = true;
+            this.CboPerfil.Items.AddRange(new object[] {
+            "Seleccione elemento",
+            "Auxiliar",
+            "Secretaria",
+            "Administrador"});
+            this.CboPerfil.Location = new System.Drawing.Point(267, 266);
+            this.CboPerfil.Name = "CboPerfil";
+            this.CboPerfil.Size = new System.Drawing.Size(121, 21);
+            this.CboPerfil.TabIndex = 8;
+            // 
+            // BtnGuardar
+            // 
+            this.BtnGuardar.Location = new System.Drawing.Point(134, 314);
+            this.BtnGuardar.Name = "BtnGuardar";
+            this.BtnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.BtnGuardar.TabIndex = 9;
+            this.BtnGuardar.Text = "Guardar";
+            this.BtnGuardar.UseVisualStyleBackColor = true;
+            this.BtnGuardar.Click += new System.EventHandler(this.BtnGuardar_Click);
+            // 
+            // BtnCancelar
+            // 
+            this.BtnCancelar.Location = new System.Drawing.Point(267, 314);
+            this.BtnCancelar.Name = "BtnCancelar";
+            this.BtnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.BtnCancelar.TabIndex = 10;
+            this.BtnCancelar.Text = "Cancelar";
+            this.BtnCancelar.UseVisualStyleBackColor = true;
+            this.BtnCancelar.Click += new System.EventHandler(this.BtnCancelar_Click);
+            // 
+            // LstUsuarios
+            // 
+            this.LstUsuarios.FormattingEnabled = true;
+            this.LstUsuarios.Location = new System.Drawing.Point(134, 363);
+            this.LstUsuarios.Name = "LstUsuarios";
+            this.LstUsuarios.Size = new System.Drawing.Size(633, 69);
+            this.LstUsuarios.TabIndex = 11;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(610, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(157, 151);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FrmUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.LstUsuarios);
+            this.Controls.Add(this.BtnCancelar);
+            this.Controls.Add(this.BtnGuardar);
+            this.Controls.Add(this.CboPerfil);
+            this.Controls.Add(this.LblPerfil);
+            this.Controls.Add(this.TxtCorreo);
+            this.Controls.Add(this.TxtPassword);
+            this.Controls.Add(this.TxtUsuario);
+            this.Controls.Add(this.LblCorreo);
+            this.Controls.Add(this.LblPassword);
+            this.Controls.Add(this.LblUsuario);
+            this.Controls.Add(this.Lbltitulo);
+            this.Name = "FrmUsuario";
+            this.Text = "FrmUsuario";
+            this.Load += new System.EventHandler(this.FrmUsuario_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Lbltitulo;
+        private System.Windows.Forms.Label LblUsuario;
+        private System.Windows.Forms.Label LblPassword;
+        private System.Windows.Forms.Label LblCorreo;
+        private System.Windows.Forms.TextBox TxtUsuario;
+        private System.Windows.Forms.TextBox TxtPassword;
+        private System.Windows.Forms.TextBox TxtCorreo;
+        private System.Windows.Forms.Label LblPerfil;
+        private System.Windows.Forms.ComboBox CboPerfil;
+        private System.Windows.Forms.Button BtnGuardar;
+        private System.Windows.Forms.Button BtnCancelar;
+        private System.Windows.Forms.ListBox LstUsuarios;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ My Form Demo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo_Formulario
+{
+    public partial class FrmUsuario : Form
+    {
+        public FrmUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnGuardar_Click(object sender, EventArgs e)
+        {
+            Usuario us1 = new Usuario();
+            us1.NombreUsuario = TxtUsuario.Text;
+            us1.Password = us1.GetSHA1(TxtPassword.Text);
+            us1.Email = TxtCorreo.Text;
+            us1.Perfil = CboPerfil.SelectedItem.ToString();
+            us1.GuardarUsuario();//Crear la logica 
+            us1.ConsultarUsuario();
+                
+
+            string usuario;
+            usuario = us1.NombreUsuario + ", " +
+               us1.Password + ", " +
+                us1.Password + ", " + us1.Perfil;
+
+            LstUsuarios.Items.Add(usuario);
+            TxtUsuario.Clear();
+            TxtPassword.Clear();
+            TxtCorreo.Clear();
+            CboPerfil.SelectedIndex = 0;
+         }
+
+        private void BtnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FrmUsuario_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
